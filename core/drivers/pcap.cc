4e46f9cfa3c2d0f9ed032a774f4e877dc2b33ec8
Chang Lan <clan@eecs.berkeley.edu>
2017-05-22 15:16:16 -0700

    pcap: Support jumbo frame

--- core/drivers/pcap.cc
+++ core/drivers/pcap.cc
@@ -1,5 +1,8 @@
 #include "pcap.h"
 
+#include <algorithm>
+#include <string>
+
 #include "../utils/pcap.h"
 
 CommandResponse PCAPPort::Init(const bess::pb::PCAPPortArg& arg) {
@@ -47,27 +50,27 @@ int PCAPPort::RecvPackets(queue_t qid, bess::Packet** pkts, int cnt) {
       break;
     }
 
-    if (caplen <= SNBUF_DATA) {
-      // pcap packet will fit in the mbuf, go ahead and copy.
-      bess::utils::CopyInlined(sbuf->append(caplen), packet, caplen, true);
-    } else {
-/* FIXME: no support for chained mbuf for now */
-#if 0
-      /* Try read jumbo frame into multi mbufs. */
-      if (unlikely(pcap_rx_jumbo(sbuf->mbuf.pool,
-              &sbuf->mbuf,
-              packet,
-              caplen) == -1)) {
-        //drop all the mbufs.
-        bess::Packet::Free(sbuf);
-        break;
-      }
-#else
-      RTE_LOG(ERR, PMD, "Dropping PCAP packet: Size (%d) > max size (%d).\n",
-              sbuf->total_len(), SNBUF_DATA);
-      bess::Packet::Free(sbuf);
-      break;
-#endif
+    int copy_len = std::min(caplen, static_cast<int>(sbuf->tailroom()));
+    bess::utils::CopyInlined(sbuf->append(copy_len), packet, copy_len, true);
+
+    packet += copy_len;
+    caplen -= copy_len;
+    bess::Packet* m = sbuf;
+
+    while (caplen > 0) {
+      m->set_next(bess::Packet::Alloc());
+      m = m->next();
+
+      // no headroom needed in chained mbufs
+      m->prepend(m->headroom());
+      m->set_data_len(0);
+      m->set_buffer(0);
+
+      copy_len = std::min(caplen, static_cast<int>(m->tailroom()));
+      bess::utils::CopyInlined(m->append(copy_len), packet, copy_len, true);
+
+      packet += copy_len;
+      caplen -= copy_len;
     }
 
     pkts[recv_cnt] = sbuf;
@@ -79,7 +82,7 @@ int PCAPPort::RecvPackets(queue_t qid, bess::Packet** pkts, int cnt) {
 
 int PCAPPort::SendPackets(queue_t, bess::Packet** pkts, int cnt) {
   if (!pcap_handle_.is_initialized()) {
-    return 0;  // TODO: Would like to raise an error here...
+    CHECK(0);  // raise an error
   }
 
   int sent = 0;
